de63b735ec8bcfc47a37744e2fe2857e29de529d
Eldon Koyle <ekoyle@gmail.com>
2016-03-23 10:12:27 -0600

    Add initial DHCP foo

---
+++ openipam-dhcp-server/openipam_config/dhcp.py
@@ -0,0 +1,15 @@
+# The default DHCP lease time for all statically assigned addresses. Dynamic
+# lease times are configured on the pool.
+
+import os
+
+static_lease_time = 86400
+
+# The IP address of this server
+listen_address = os.environ.get('listen_address', '192.168.0.1')
+listen_interface = os.environ.get('listen_interface', 'eth0')
+
+server_listen = [{'address': listen_address, 'interface': listen_interface, 'broadcast': True, 'unicast': True, }]
+
+# DHCP options that should always be returned if they are defined
+force_options = [66, 67]
